b465bdc58d97ff0c2a0d7aedb7747da022d2f507
Griffin McLennan <mclennangriffin@gmail.com>
2020-09-14 20:50:01 -0500

    Basic react-router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-        <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="app">
+            {/* Header Component */}
+
+            <Router>
+                <Switch>
+                    <Route path="/:creatorName">
+                        <h1>Welcome to a creator Page</h1>
+                    </Route>
+                    <Route path="/">
+                        <h1>Homepage</h1>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
