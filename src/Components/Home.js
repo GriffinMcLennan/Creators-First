10b4ffa3372343086631b3246c9c1fad3ca5b8c8
Griffin McLennan <mclennangriffin@gmail.com>
2020-09-16 01:10:24 -0500

    Updated signin for homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
+import creatorImage1 from "./homepage_images/homepage_creator1.jpg";
+import TextField from "@material-ui/core/TextField";
 
 function Home() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div className="home">
             <div className="home__welcome">
@@ -10,6 +19,37 @@ function Home() {
                     The premier platform for connecting your audience with
                     exclusive content.
                 </h3>
+
+                <div className="home__body">
+                    <img src={creatorImage1} />
+
+                    <form className="home__input" onSubmit={(e) => login(e)}>
+                        <h2>Login or Signup!</h2>
+                        <TextField
+                            id="outlined-basic"
+                            label="Username"
+                            variant="outlined"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <div className="home__login">
+                            <div className="home__loginNormal">
+                                <button>Login</button>
+                                <button>Signup</button>
+                            </div>
+
+                            <div className="home__googleLogin">
+                                <button>google signin</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
 
                 <h3>
                     At Creators First you receive 10% more money than from the
