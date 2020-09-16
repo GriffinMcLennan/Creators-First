17a3730d40084701c61a2da2a8415bb58eb8c684
Griffin McLennan <mclennangriffin@gmail.com>
2020-09-16 00:38:16 -0500

    Re-modelled Post structure and css, added image

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Post.css";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
 
 function Post({ info: { title, imgURL, description, likes } }) {
+    const [likesCnt, setLikesCnt] = useState(0);
+    const [liked, setLiked] = useState(false);
+
+    const handleLike = () => {
+        if (!liked) {
+            setLikesCnt((numLikes) => numLikes + 1);
+        } else {
+            setLikesCnt((numLikes) => numLikes - 1);
+        }
+
+        setLiked(!liked);
+    };
+
+    useEffect(() => {
+        setLikesCnt(likes);
+    }, []);
+
     return (
         <div className="post">
             <h1>{title}</h1>
             <img src={imgURL} />
-            <h3>{description}</h3>
-            <h4>
-                <ThumbUpAltIcon /> {likes}
-            </h4>
+            <div className="post__info">
+                <p>{description}</p>
+
+                <div className="post__likes">
+                    <div className="post__like" onClick={handleLike}>
+                        {!liked ? (
+                            <ThumbUpAltOutlinedIcon />
+                        ) : (
+                            <ThumbUpAltIcon />
+                        )}
+                        <p>{likesCnt}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
