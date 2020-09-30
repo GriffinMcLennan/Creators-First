2f1b5a7c3f2f88e91667a158648a066a6219162e
Griffin McLennan <mclennangriffin@gmail.com>
2020-09-29 23:48:42 -0500

    Added inputs for register page

--- src/Components/RegisterPage.js
+++ src/Components/RegisterPage.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./RegisterPage.css";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function RegisterPage() {
     const history = useHistory();
     const state = useSelector((state) => state);
     const isSignedIn = state.user.username !== null;
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     useEffect(() => {
         if (isSignedIn) {
@@ -16,9 +20,40 @@ function RegisterPage() {
 
     return (
         <div className="registerpage">
-            <h1>Register</h1>
+            <h2>Sign-up for a Creators First Account</h2>
+
+            <div className="registerpage__input">
+                <TextField
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                    onChange={(e) => setUsername(e.target.value)}
+                    style={TextFieldStyle}
+                />
+
+                <TextField
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={TextFieldStyle}
+                />
+            </div>
+
+            <Button variant="contained" color="primary">
+                Sign-Up
+            </Button>
         </div>
     );
 }
+
+const TextFieldStyle = {
+    width: "30vw",
+    minWidth: "200px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "20px",
+};
 
 export default RegisterPage;
